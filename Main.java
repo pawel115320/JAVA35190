a163449f7d3f073ae84db4d7d0ade5ff9b17f29f
pawel115320 <56114087+pawel115320@users.noreply.github.com>
2021-03-16 11:36:42 +0100

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,20 @@
+package pl.lublin.wsei.java.cwiczenia;
+//** xxx */
+
+public class Main {
+
+    public static void main(String[] args) {
+      Scanner input = new Scanner (System.in);
+      int num1,num2;
+      System.out.print("Podaj pierwsza liczbe: ");
+      num1 = input.nextInt();
+      System.out.print("Podaj druga liczbe: ");
+      num2 = input.nextInt();
+      System.out.print("Wynik dodawania %d + %d = %d%n",num1,num2,num1+num2);
+
+
+    // write your code here
+        // xxx
+        // * xxx */
+    }
+}
